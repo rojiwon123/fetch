82c60d15f22f55615fae00df993ac4f2728c3c05
rojiwon <dev.rojiwon@gmail.com>
2024-05-03 19:06:33 +0900

    feat: add response parser

--- src/response.ts
+++ src/response.ts
@@ -10,44 +10,46 @@ export interface IFetchResponse<IBody, Status> {
 
 const base =
     <T, Status>(options: {
+        status?: Status | Status[];
         content_type: string;
-        parser: (res: IResponse) => Promise<T>;
-        validator?: ((input: T) => input is T) | ((input: T) => boolean);
-        status?: Status;
+        parse: (res: IResponse) => Promise<unknown>;
+        is?: (input: unknown) => input is T;
     }) =>
     async (res: IResponse): Promise<IFetchResponse<T, Status>> => {
-        const headers = fromHeaders(res.headers);
-        const cause = {
-            url: res.url,
-            status: res.status,
-            headers,
-        };
-        if (typeof options.status === "number" && options.status !== res.status)
-            throw new FetchError("Invalid Status", options, cause);
-        if (!res.headers.get("content-type")?.startsWith(options.content_type))
+        const {
+            status,
+            content_type,
+            parse,
+            is = (input): input is T => true,
+        } = options;
+        const mismatchStatus =
+            typeof status === "number" && status !== res.status;
+        const excludeStatus =
+            Array.isArray(status) &&
+            !status.some((status) => status === res.status);
+        const failyStatus = status === undefined && res.status >= 400;
+        if (mismatchStatus || excludeStatus || failyStatus)
+            throw new FetchError("Invalid Status", options);
+        if (!res.headers.get("content-type")?.startsWith(content_type))
             throw new FetchError(
                 "Invalid Response Body",
                 "response body content-type not matched.",
                 options,
-                cause,
             );
         const body = await FetchError.wrap(
             "Invalid Response Body",
-            options.parser,
+            parse,
             "fail to parse response body",
-            cause,
         )(res);
-        if (options.validator && !options.validator(body))
+        if (!is(body))
             throw new FetchError(
                 "Invalid Response Body",
                 "fail to validate response body",
-                cause,
             );
-
         return {
-            body,
             status: res.status as Status,
-            headers,
+            headers: fromHeaders(res.headers),
+            body,
         };
     };
 
@@ -56,28 +58,28 @@ const json = <
     Status extends number,
 >(
     options: {
-        validator?: (input: T) => input is T;
         status?: Status;
+        is?: (input: unknown) => input is T;
     } = {},
 ) =>
     base({
         status: options.status,
-        parser: (res) => res.json(),
-        validator: options.validator,
         content_type: "application/json",
+        parse: (res) => res.json(),
+        is: options.is,
     });
 
 const text = <T extends string, Status extends number>(
     options: {
-        content_type?: `text/${string}`;
         status?: Status;
-        validator?: (input: string) => input is T;
+        content_type?: `text/${string}`;
+        is?: (input: unknown) => input is T;
     } = {},
 ) =>
     base({
         status: options.status,
-        parser: (res) => res.text(),
-        validator: options.validator,
+        parse: (res) => res.text(),
+        is: options.is,
         content_type: options.content_type ?? "text/plain",
     });
 
